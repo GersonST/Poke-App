010625bc109920bca86ca57aef49d6a74b442c77
GeffersonST <geffsevero@gmail.com>
2021-09-29 19:26:04 -0300

    Aplicando a API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = res.json();
+
+    console.log(data);
+  };
+
   return (
-    <div className="App">
+    <div className="app-container">
       <h1> Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+      </div>
+      <button className="load-more">Load more</button>
     </div>
   );
 }
